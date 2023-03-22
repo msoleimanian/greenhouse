03b1a770c699d380185fa750a09eceeba046cd1d
msoleimanian <36849205+msoleimanian@users.noreply.github.com>
2023-03-22 14:19:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
 
 
 
 st.title("Green House Demo")
 st.text("This is a Demo for Greenhouse")
-
-st.sidebar.title("Navigation")
-Options = st.sidebar.radio('Sections' , options=('Home', 'Upload' , 'pak choy greenhouse'))
