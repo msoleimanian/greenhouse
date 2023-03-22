f7d5f93819c4303554138d0586e6012b06e03d36
msoleimanian <36849205+msoleimanian@users.noreply.github.com>
2023-03-22 14:19:53 +0800

    Delete 01_home.py

--- 01_home.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
-
-if __name__ == '__main__':
-    st.title("Green House Demo")
-    st.text("This is a Demo for upload files and ")
